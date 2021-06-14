e5f53e468949cf622c83e266fc2cf9a4016f9ee7
Maksim Binger <maksim.binger@vattenfall.com>
2021-06-14 09:48:32 +0200

    Add a windows application

---
+++ App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slideshow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wp = new Wallpaper();
+            wp.SetSlideShow(@"C:\Wallpapers\Wallpapers", TimeSpan.FromMinutes(10), Wallpaper.DesktopWallpaperPosition.Stretch);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # EnableWindows10Slideshow
 A powershell script to enable windows 10 slideshow when it being disabled
+Alternative a Windows Application when PowerShell is disabled
